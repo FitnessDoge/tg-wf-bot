201eea1fccbd4e1c3d20bc1556e64786bef88d7b
Techjoy <930763179@qq.com>
2024-05-17 22:31:53 +0800

    add feature: province menu, city menu, county menu

---
+++ README.md
@@ -0,0 +1,5 @@
+# Telegram Weather Forecast Bot
+
+start - Introduction
+forecast - Weather forecast of tomorrow
+forecast3 - Weather forecast of the next three days
--- src/main/java/tgbot/tgwfbot/bot/WeatherForecastBot.java
+++ src/main/java/tgbot/tgwfbot/bot/WeatherForecastBot.java
@@ -6,9 +6,15 @@ import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsume
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
@@ -36,7 +42,24 @@ public class WeatherForecastBot implements LongPollingSingleThreadUpdateConsumer
         if (update.hasMessage()) {
             message = update.getMessage();
         } else if (update.hasCallbackQuery()) {
-            // TODO: handle callback
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String data = callbackQuery.getData();
+            Long chatId = callbackQuery.getMessage().getChatId();
+            Integer messageId = callbackQuery.getMessage().getMessageId();
+
+            String[] split = data.split("=");
+            if (split.length == 2) {
+                if ("province".equals(split[0])) {
+                    sendCityMenu(chatId, messageId);
+                } else if ("city".equals(split[0])) {
+                    sendCountyMenu(chatId, messageId);
+                } else if ("county".equals(split[0])) {
+                    // TODO query forecast
+                    System.out.println("County code => " + split[1]);
+                }
+            } else {
+                System.out.println("Unknown error...");
+            }
             return;
         } else {
             return;
@@ -48,19 +71,116 @@ public class WeatherForecastBot implements LongPollingSingleThreadUpdateConsumer
 
         if (message.isCommand()) {
             String command = message.getText();
-            if ("/start".equals(command)) {
-                String answerMessage = "Hello,there is Techjoy bot. I'll give you the weather forecast.";
-                sendSimpleMessage(chatId, answerMessage);
-            } else if ("/forecast".equals(command)) {
-                // TODO Forecast for tomorrow
-            } else if ("/forecast3".equals(command)) {
-                // TODO Forecast for the incoming three days
-            }
+            handleCommand(command, chatId);
         } else {
             String answerMessage = "Please enter the correct command.";
             sendSimpleMessage(chatId, answerMessage);
         }
 
+    }
+
+    private void sendCountyMenu(Long chatId, Integer messageId) {
+        InlineKeyboardButton yb = InlineKeyboardButton.builder()
+                .text("渝北区")
+                .callbackData("county=500112")
+                .build();
+        InlineKeyboardButton na = InlineKeyboardButton.builder()
+                .text("南岸区")
+                .callbackData("county=500108")
+                .build();
+        InlineKeyboardButton jb = InlineKeyboardButton.builder()
+                .text("江北区")
+                .callbackData("county=500105")
+                .build();
+        InlineKeyboardMarkup keyboardMarkup = InlineKeyboardMarkup.builder()
+                .keyboardRow(new InlineKeyboardRow(yb, na, jb))
+                .build();
+        EditMessageReplyMarkup editMessageReplyMarkup = EditMessageReplyMarkup.builder()
+                .chatId(chatId)
+                .messageId(messageId)
+                .replyMarkup(keyboardMarkup)
+                .build();
+        EditMessageText editMessageText = EditMessageText.builder()
+                .chatId(chatId)
+                .messageId(messageId)
+                .text("Please select the county you want to query: ")
+                .build();
+        try {
+            client.execute(editMessageText);
+            client.execute(editMessageReplyMarkup);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendCityMenu(Long chatId, Integer messageId) {
+        InlineKeyboardButton cq = InlineKeyboardButton.builder()
+                .text("重庆市")
+                .callbackData("city=500000")
+                .build();
+        InlineKeyboardMarkup keyboardMarkup = InlineKeyboardMarkup.builder()
+                .keyboardRow(new InlineKeyboardRow(cq))
+                .build();
+        EditMessageReplyMarkup editMessageReplyMarkup = EditMessageReplyMarkup.builder()
+                .chatId(chatId)
+                .messageId(messageId)
+                .replyMarkup(keyboardMarkup)
+                .build();
+        EditMessageText editMessageText = EditMessageText.builder()
+                .chatId(chatId)
+                .messageId(messageId)
+                .text("Please select the city you want to query: ")
+                .build();
+        try {
+            client.execute(editMessageText);
+            client.execute(editMessageReplyMarkup);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleCommand(String command, Long chatId) {
+        switch (command) {
+            case "/start" -> {
+                String answerMessage = "Hello,there is Techjoy bot. I'll give you the weather forecast.";
+                sendSimpleMessage(chatId, answerMessage);
+            }
+            case "/forecast" -> {
+                sendProvinceMenu(chatId);
+            }
+            case "/forecast3" -> {
+                // TODO Forecast for the incoming three days
+                sendProvinceMenu(chatId);
+            }
+            case null, default -> {
+                String answerMessage = "Command is currently not supported.";
+                sendSimpleMessage(chatId, answerMessage);
+            }
+        }
+    }
+
+    private void sendProvinceMenu(Long chatId) {
+        InlineKeyboardButton bj = InlineKeyboardButton.builder()
+                .text("北京")
+                .callbackData("province=110000")
+                .build();
+        InlineKeyboardButton cq = InlineKeyboardButton.builder()
+                .text("重庆")
+                .callbackData("province=500000")
+                .build();
+        InlineKeyboardMarkup keyboardMarkup = InlineKeyboardMarkup.builder()
+                .keyboardRow(new InlineKeyboardRow(bj, cq))
+                .build();
+        SendMessage sendMessage = SendMessage.builder()
+                .chatId(chatId)
+                .text("Please select the province you want to query:")
+                .replyMarkup(keyboardMarkup)
+                .build();
+        try {
+            client.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     private void sendSimpleMessage(Long chatId, String answerMessage) {
